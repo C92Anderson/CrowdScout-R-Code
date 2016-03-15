2bc8db2f4d32b0fb0c34623da57ac4bde7b803f8
Cole Anderson <cole92anderson@gmail.com>
2016-03-15 12:18:00 -0700

    hockey-master-table

--- ui.R
+++ ui.R
@@ -1,47 +1,43 @@
 library(shiny)
 library(ggplot2)
 
-shinyUI(fluidPage(
-
-  plotOutput('myplot'),
-  
-  fluidRow(
-    column(5, offset = 1,
-          h5('Compare Daily Elo Ratings of Selected Players'),
-       ##       br(),
-              uiOutput('p1sel'),
-              uiOutput('p2sel')
-          
-           ),
-    column(5, offset = 1,
-           dateInput('start_dt',
-                     label = paste('Start Date:'),
-                     value = Sys.Date() -90),
-           br(),
-           h5('Scout at http://www.crowdscoutsports.com'),
-           h5('Follow @crowdscoutsprts'))
-          )
-  
+shinyUI(
+  fluidPage(
+    h5('Scout at http://www.crowdscoutsports.com'),
+    h5('Follow @crowdscoutsprts'),
     
-## headerPanel('Player Compare Tool'),
-
-##  sidebarPanel(
-##  
-##    helpText('Compare Daily Elo Ratings of Selected Players'),
-##    
-##    uiOutput('p1sel'),
-##    uiOutput('p2sel'),
-    
-##    helpText('Scout at http://www.crowdscoutsports.com'),
-##    helpText('Follow @crowdscoutsprts'),
-    
-##    dateInput('start_dt',
-##              label = paste('Start Date:'),
-##              value = Sys.Date() -40
-##    )
-    
-#3  ),
-##    mainPanel(plotOutput('myplot')
-##    )
-     
-))
+    # Create a new Row in the UI for selectInputs
+    fluidRow(
+      column(4, 
+             uiOutput('team')
+      ),
+      column(4, 
+             uiOutput('draft_year')
+      ),
+      column(4, 
+             uiOutput('position') 
+      )        
+    ),
+    ###row 2
+##    fluidRow(
+##     column(4, 
+##             sliderInput("age", "Age (16-Jagr):",
+##                         min = 16, max = 50, value = c(16,50))
+##      ),
+##      column(4, 
+##             sliderInput("Draft Year", 
+##                         "Draft Year:", 
+##                         min = 1990, max = 2015, value = c(1990,2015))
+##      ),
+##      column(4, 
+##             sliderInput("elo", 
+##                         "Current Elo:", 
+##                         min = 1000, max = 2000, value = c(1000,2000))
+##      )        
+##    ),  
+    # Create a new row for the table.
+    fluidRow(
+      dataTableOutput(outputId="table")
+    )    
+  )  
+)
